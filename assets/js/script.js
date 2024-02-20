1667ebde414dd9798cfdea70d5465bd82a28d40c
john furlong <furlongjohn088@gmail.com>
2024-02-20 13:15:33 +0000

    added references to html elements

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,9 @@
 // Options for the game 
 let options = ["rock","paper","scissors"];
 
+// References to HTML elements
+let computerDisplay = document.getElementById("computerDisplay");
+let userDisplay = document.getElementById("userDisplay");
+let computerScoreDisplay = document.getElementById("computerScoreDisplay");
+let userScoreDisplay = document.getElementById("userScoreDisplay");
+let resultDisplay = document.getElementById("resultDisplay");
